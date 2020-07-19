5ce3e277db8abd6e36a5fac1b54793a2ffec620a
David <dwebster182@gmail.com>
2020-07-19 11:18:43 +0200

    Update KnightMain.java

--- src/main/java/sia/knights/KnightMain.java
+++ src/main/java/sia/knights/KnightMain.java
@@ -8,8 +8,8 @@ public class KnightMain {
   public static void main(String[] args) throws Exception {
 //    ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("META-INF/spring/knight.xml");
 
-    AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(KnightConfig.class);
-    Knight knight = context.getBean(Knight.class);
+    AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(KnightConfig.class);
+    Knight knight = ctx.getBean(Knight.class);
     knight.embarkOnQuest();
 
   }
